67e0fe21b1b886748d166fa258c0937af700c357
Vishal <vishalaravind03@gmail.com>
2024-05-02 09:20:52 +0530

    Added user profile feed api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -10,31 +10,41 @@ class HelloSerializer(serializers.Serializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     """Serializes a user profile object"""
-
     class Meta:
         model = models.UserProfile
         fields = ('id', 'email', 'name', 'password')
-        extra_kwargs= {
+        extra_kwargs = {
             'password': {
                 'write_only': True,
-                'style': {'input_type': 'password'}
+                'style': {
+                    'input_type': 'password'
+                }
             }
         }
 
-        def create(self, validated_data):
-            """Create and return a new user"""
-            user = models.UserProfile.objects.create_user(
-                email=validated_data['email'],
-                name=validated_data['name'],
-                password =validated_data['password']
-            )
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
 
-            return user
-        
-        def update(self, instance, validated_data):
-            """Handle updating user account"""
-            if 'password' in validated_data:
-                password = validated_data.pop('password')
-                instance.set_password(password)
+        return user
 
-            return super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profiles', 'status_text', 'created_on')
+        extra_kwargs = {'user_profiles': {'read_only': True}}
